2cdc152c0e420c0db8def56c4eca3eb922dd232a
Stuart Zong <szong@bcgsc.ca>
2019-06-27 17:39:13 -0700

    add pagination

--- microblog/config.py
+++ microblog/config.py
@@ -12,3 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['szong@bcgsc.ca']
+    POSTS_PER_PAGE = 25
